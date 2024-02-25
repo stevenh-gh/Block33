62921dfa9cdfe5c5a7d40418e89978c9868d1894
steven huynh <steven.h@live.com>
2024-02-25 11:46:14 -0800

    finish routes

--- server/index.js
+++ server/index.js
@@ -8,20 +8,94 @@ app.use(require("morgan")("dev"));
 
 app.get("/api/employees", async (req, res, next) =>
 {
-	let sql = `
-		select * from employee;
-	`;
-	const response = await client.query(sql);
-	res.send(response.rows);
+	try
+	{
+		let sql = `
+			select * from employee;
+		`;
+		const response = await client.query(sql);
+		res.send(response.rows);
+	}
+	catch (error)
+	{
+		next(error);
+	}
 });
 
 app.get("/api/departments", async (req, res, next) =>
 {
-	let sql = `
-		select * from department;
-	`;
-	const response = await client.query(sql);
-	res.send(response.rows);
+	try
+	{
+		let sql = `
+			select * from department;
+		`;
+		const response = await client.query(sql);
+		res.send(response.rows);
+	}
+	catch (error)
+	{
+		next(error);
+	}
+});
+
+app.post("/api/employees", async (req, res, next) =>
+{
+	try
+	{
+		let sql = `
+			insert into employee
+				(name, department_id)
+			values
+				($1, $2)
+			returning *;
+		`;
+		const response = await client.query(sql, [req.body.name, req.body.department_id]);
+		res.send(response.rows[0]);
+	}
+	catch (error)
+	{
+		next(error);
+	}
+});
+
+app.delete("/api/employees/:id", async (req, res, next) =>
+{
+	try
+	{
+		let sql = `
+			delete from employee
+			where id = $1;
+		`;
+		await client.query(sql, [req.params.id]);
+		res.sendStatus(204);
+	}
+	catch (error)
+	{
+		next(error);
+	}
+});
+
+app.put("/api/employees/:id", async (req, res, next) =>
+{
+	try
+	{
+		let sql = `
+		update employee
+		set
+			name = $1,
+			department_id = $2,
+			updated_at = now()
+		where
+			id = $3
+		returning *;
+		`;
+		const response = await client.query(sql, [req.body.name, req.body.department_id, req.params.id]);
+		res.send(response.rows[0]);
+	}
+	catch (error)
+	{
+		next(error);
+	}
 });
 
 const init = async () =>
@@ -36,7 +110,7 @@ const init = async () =>
 			id serial primary key,
 			name varchar(255) not null
 		);
-	`;
+		`;
 	await client.query(sql);
 	console.log("created department table");
 
@@ -50,7 +124,7 @@ const init = async () =>
 			updated_at timestamp default now(),
 			department_id integer references department(id) not null
 		);
-	`;
+		`;
 	await client.query(sql);
 	console.log("created employee table");
 
@@ -69,7 +143,7 @@ const init = async () =>
 			('steven', 1),
 			('employee2', 2),
 			('employee3', 3);
-	`;
+		`;
 	await client.query(sql);
 	console.log("seeded db");
 
